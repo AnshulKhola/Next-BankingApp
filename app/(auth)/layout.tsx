cdb288e8d6d5dc608c0206573c119555238d5cbc
AnshulKhola <golukhola18@gmail.com>
2024-06-15 16:52:31 +0530

    Changed logo and Auth-Image

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -9,16 +9,17 @@ export default function RootLayout({
     <main className="flex min-h-screen w-full justify-between font-inter">
       {children}
       <div className="auth-asset">
-        <div>
+
+        <div className="border-gray-900 border-4">
           <Image 
-            src="/icons/auth-image.svg"
+            src="/icons/auth-image.png"
             alt="Auth image"
-            width={500}
-            height={500}
+            width={600}
+            height={600}
             className="rounded-l-xl object-contain"
           />
         </div>
-      </div>
+        </div>
     </main>
   );
 }
